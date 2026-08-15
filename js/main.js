44e6b2025679dbfce9191e517ce67060997044f4
bhupatijagi12 <bhupati.jagi@gmail.com>
2026-08-15 19:12:33 +0530

    main js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-
+$(function(){
+  $('.navbar-nav a').on('click',function(){$('.navbar-collapse').collapse('hide');});
+  function reveal(){ $('.reveal').each(function(){if($(this).offset().top < $(window).scrollTop()+$(window).height()-60)$(this).addClass('visible');}); }
+  $(window).on('scroll resize',reveal); reveal();
+  $('.counter').each(function(){var $t=$(this),target=+$t.data('target');$({n:0}).animate({n:target},{duration:1600,easing:'swing',step:function(){$t.text(Math.ceil(this.n));},complete:function(){$t.text(target);}});});
+  $('form').on('submit',function(e){e.preventDefault();$(this).find('.status').hide().html('<span class="text-success fw-bold">Thank you. Your demonstration request has been recorded.</span>').fadeIn(500);this.reset();});
+  $('.year').text(new Date().getFullYear());
+});
